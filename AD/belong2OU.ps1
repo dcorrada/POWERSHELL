a5cbe9125df0cabac2ac0502229fa2ce4203cde4
Dario CORRADA <dario.corrada@agmsolutions.net>
2020-12-16 13:24:17 +0100

    Belong2OU aggiornato

--- AD/belong2OU.ps1
+++ AD/belong2OU.ps1
@@ -4,16 +4,16 @@ Version...: 19.04.1
 Author....: Dario CORRADA
 
 Questo script accede ad Active Directory e fornisce la lista dei computer che appartengono ad una OU
-
-
-+++ UPDATES +++
-
-[2019-04-15 Dario CORRADA] 
-Primo rilascio
-
 #>
 
+# header
 $ErrorActionPreference= 'Inquire'
+$WarningPreference = 'SilentlyContinue'
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+Add-Type -AssemblyName PresentationFramework
+
+Import-Module -Name '\\192.168.2.251\Dario\SCRIPT\Moduli_PowerShell\Forms.psm1'
 
 # setto le policy di esecuzione degli script
 $ErrorActionPreference= 'SilentlyContinue'
@@ -22,8 +22,7 @@ Write-Host "ExecutionPolicy Bypass" -fore Green
 $ErrorActionPreference= 'Inquire'
 
 # Controllo accesso
-Import-Module -Name '\\itmilitgroup\SD_Utilities\SCRIPT\Moduli_PowerShell\Patrol.psm1'
-$login = Patrol -scriptname belong2OU
+$login = LoginWindow
 
 # Importo il modulo di Active Directory
 if (! (get-Module ActiveDirectory)) { Import-Module ActiveDirectory } 
@@ -33,14 +32,15 @@ $ou2find = Read-Host "OU in cui cercare"
 Write-host "Recupero la lista delle OU disponibili..."
 $ou_available = Get-ADOrganizationalUnit -Filter *
 
+$suffix = ",DC=agm,DC=local"
 if ($ou_available.Name -contains $ou2find) {
     Write-Host "Ricerca computer appartenenti a [$ou2find]"
-    $string = "OU=" + $ou2find + ",DC=it,DC=kworld,DC=kpmg,DC=com"
+    $string = "OU=" + $ou2find + $suffix
     $ErrorActionPreference= 'SilentlyContinue'
     $computer_list = Get-ADComputer -Filter * -SearchBase $string
     $ErrorActionPreference= 'Inquire'
     if ($computer_list -eq $null) {
-        $string = "CN=" + $ou2find + ",DC=it,DC=kworld,DC=kpmg,DC=com"
+        $string = "CN=" + $ou2find + $suffix
         $computer_list = Get-ADComputer -Filter * -SearchBase $string
     }
     $computer_list.Name >> "C:\Users\$env:USERNAME\Desktop\OU_$ou2find.log"
