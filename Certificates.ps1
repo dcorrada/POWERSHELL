6e103a10f4fdbf4eb6850f956ad276154542c3f3
Dario CORRADA <dario.corrada@agmsolutions.net>
2021-01-03 23:41:02 +0100

    update

--- Certificates.ps1
+++ Certificates.ps1
@@ -1,12 +1,12 @@
 ﻿<#
-Name......: Certificati.ps1
+Name......: Certificates.ps1
 Version...: 20.12.1
 Author....: Dario CORRADA
 
-Questo script richiede o rimuove i certificati utente
+This script request or remove certificates
 #>
 
-# faccio in modo di elevare l'esecuzione dello script con privilegi di admin
+# elevated script execution with admin privileges
 $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
 $testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 if ($testadmin -eq $false) {
@@ -14,9 +14,9 @@ if ($testadmin -eq $false) {
     exit $LASTEXITCODE
 }
 
-# recupero il percorso di installazione
+# get working directory
 $fullname = $MyInvocation.MyCommand.Path
-$fullname -match "([a-zA-Z_\-\.\\\s0-9:]+)\\Certificati\.ps1$" > $null
+$fullname -match "([a-zA-Z_\-\.\\\s0-9:]+)\\Certificates\.ps1$" > $null
 $workdir = $matches[1]
 
 # header 
@@ -28,24 +28,24 @@ $WarningPreference = 'SilentlyContinue'
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 Add-Type -AssemblyName PresentationFramework
-Import-Module -Name "$workdir\Moduli_PowerShell\Forms.psm1"
+Import-Module -Name "$workdir\Modules\Forms.psm1"
 
-# form di scelta 
-$form_modalita = FormBase -w 300 -h 200 -text "CERTIFICATI"
-$togli = RadioButton -form $form_modalita -checked $true -x 30 -y 20 -text "Rimuovi certificato"
-$metti  = RadioButton -form $form_modalita -checked $false -x 30 -y 60 -text "Richiedi certificato"
+# dialog box
+$form_modalita = FormBase -w 300 -h 200 -text "CERTIFICATES"
+$togli = RadioButton -form $form_modalita -checked $true -x 30 -y 20 -text "Remove certificate"
+$metti  = RadioButton -form $form_modalita -checked $false -x 30 -y 60 -text "Request certificate"
 OKButton -form $form_modalita -x 90 -y 120 -text "Ok"
 $result = $form_modalita.ShowDialog()
 
 if ($result -eq "OK") {
     if ($togli.Checked) {
-        # elenco i certificati installati
+        # list installed certificates
         $cert_list = Get-ChildItem cert:CurrentUser\My
         $opt_list = @()
         foreach ($cert in $cert_list) {
             $opt_list += $cert.Subject
         }
-        $formlist = FormBase -w 400 -h 200 -text "CERTIFICATI"
+        $formlist = FormBase -w 400 -h 200 -text "CERTIFICATES"
         $DropDown = new-object System.Windows.Forms.ComboBox
         $DropDown.Location = new-object System.Drawing.Size(10,60)
         $DropDown.Size = new-object System.Drawing.Size(350,30)
@@ -58,14 +58,14 @@ if ($result -eq "OK") {
         $DropDownLabel = new-object System.Windows.Forms.Label
         $DropDownLabel.Location = new-object System.Drawing.Size(10,20) 
         $DropDownLabel.size = new-object System.Drawing.Size(500,30) 
-        $DropDownLabel.Text = "Scegliere il certificato"
+        $DropDownLabel.Text = "Select certificate"
         $formlist.Controls.Add($DropDownLabel)
         OKButton -form $formlist -x 100 -y 100 -text "Ok"
         $formlist.Add_Shown({$DropDown.Select()})
         $result = $formlist.ShowDialog()
         $selected = $DropDown.Text
 
-        # rimuovo il certificato
+        # remove certificate
         foreach ($cert in $cert_list) { 
             if ($cert.Subject -match $selected) {
                 Remove-Item -path $cert.PSPath -recurse -Force
@@ -73,8 +73,8 @@ if ($result -eq "OK") {
         }
 
     } elseif ($metti.Checked) {
-        # elenco i certificati disponibili
-        $formlist = FormBase -w 400 -h 200 -text "CERTIFICATI"
+        # list available certificates
+        $formlist = FormBase -w 400 -h 200 -text "CERTIFICATES"
         $DropDown = new-object System.Windows.Forms.ComboBox
         $DropDown.Location = new-object System.Drawing.Size(10,60)
         $DropDown.Size = new-object System.Drawing.Size(350,30)
@@ -87,7 +87,7 @@ if ($result -eq "OK") {
         $DropDownLabel = new-object System.Windows.Forms.Label
         $DropDownLabel.Location = new-object System.Drawing.Size(10,20) 
         $DropDownLabel.size = new-object System.Drawing.Size(500,30) 
-        $DropDownLabel.Text = "Scegliere il certificato"
+        $DropDownLabel.Text = "Select certificate"
         $formlist.Controls.Add($DropDownLabel)
         OKButton -form $formlist -x 100 -y 100 -text "Ok"
         $formlist.Add_Shown({$DropDown.Select()})
@@ -100,7 +100,7 @@ if ($result -eq "OK") {
             $tag = "EFS"
         }
 
-        # Leggersi la documentazione relativa al comando certreq su:
+        # Read documentation of cmdlet certreq:
         # https://docs.microsoft.com/it-it/windows-server/administration/windows-commands/certreq_1  
         $certreq_log = certreq -enroll -user -policyserver * $tag
     }
