1e67cf68088196cd0b1131897802fe38ee6c180e
Dario CORRADA <dario.corrada@agmsolutions.net>
2021-01-08 14:47:57 +0100

    update

--- Check_Credentials.ps1
+++ Check_Credentials.ps1
@@ -6,47 +6,51 @@ Add-Type -AssemblyName PresentationFramework
 $workdir = Get-Location
 Import-Module -Name "$workdir\Modules\Forms.psm1"
 
- $form_PWD = New-Object System.Windows.Forms.Form
- $form_PWD.Text = "LOGIN"
- $form_PWD.Size = "400,250"
- $form_PWD.StartPosition = 'CenterScreen'
- $form_PWD.Topmost = $true
- $label = New-Object System.Windows.Forms.Label
- $label.Location = New-Object System.Drawing.Size(10,20) 
- $label.Size = New-Object System.Drawing.Size(300,20) 
- $label.Text = "Insert your credentials"
- $form_PWD.Controls.Add($label)
- $usrlabel = New-Object System.Windows.Forms.Label
- $usrlabel.Location = New-Object System.Drawing.Size(10,50) 
- $usrlabel.Size = New-Object System.Drawing.Size(100,20) 
- $usrlabel.Text = "Username:"
- $form_PWD.Controls.Add($usrlabel)
- $textBox = New-Object System.Windows.Forms.TextBox
- $textBox.Location = New-Object System.Drawing.Point(130,50)
- $textBox.Size = New-Object System.Drawing.Size(150,20)
- $form_PWD.Add_Shown({$textBox.Select()})
- $form_PWD.Controls.Add($textBox)
- $pwdlabel = New-Object System.Windows.Forms.Label
- $pwdlabel.Location = New-Object System.Drawing.Size(10,80) 
- $pwdlabel.Size = New-Object System.Drawing.Size(100,20) 
- $pwdlabel.Text = "Password:"
- $form_PWD.Controls.Add($pwdlabel)
- $MaskedTextBox = New-Object System.Windows.Forms.MaskedTextBox
- $MaskedTextBox.PasswordChar = '*'
- $MaskedTextBox.Location = New-Object System.Drawing.Point(130,80)
- $MaskedTextBox.Size = New-Object System.Drawing.Size(150,20)
- $form_PWD.Add_Shown({$MaskedTextBox.Select()})
- $form_PWD.Controls.Add($MaskedTextBox)
- $OKButton = New-Object System.Windows.Forms.Button
- $OKButton.Location = "100,120"
- $OKButton.Size = '100,30'
- $OKButton.Text = "Ok"
- $OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
- $form_PWD.AcceptButton = $OKButton
- $form_PWD.Controls.Add($OKButton)
- $result = $form_PWD.ShowDialog()
+# NOTE: username needs to be typed as "[domain\]username"
+$form_PWD = New-Object System.Windows.Forms.Form
+$form_PWD.Text = "LOGIN"
+$form_PWD.Size = "400,250"
+$form_PWD.StartPosition = 'CenterScreen'
+$form_PWD.Topmost = $true
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Size(10,20) 
+$label.Size = New-Object System.Drawing.Size(300,20) 
+$label.Text = "Insert your credentials"
+$form_PWD.Controls.Add($label)
+$usrlabel = New-Object System.Windows.Forms.Label
+$usrlabel.Location = New-Object System.Drawing.Size(10,50) 
+$usrlabel.Size = New-Object System.Drawing.Size(100,20) 
+$usrlabel.Text = "Username:"
+$form_PWD.Controls.Add($usrlabel)
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(130,50)
+$textBox.Size = New-Object System.Drawing.Size(150,20)
+$form_PWD.Add_Shown({$textBox.Select()})
+$form_PWD.Controls.Add($textBox)
+$pwdlabel = New-Object System.Windows.Forms.Label
+$pwdlabel.Location = New-Object System.Drawing.Size(10,80) 
+$pwdlabel.Size = New-Object System.Drawing.Size(100,20) 
+$pwdlabel.Text = "Password:"
+$form_PWD.Controls.Add($pwdlabel)
+$MaskedTextBox = New-Object System.Windows.Forms.MaskedTextBox
+$MaskedTextBox.PasswordChar = '*'
+$MaskedTextBox.Location = New-Object System.Drawing.Point(130,80)
+$MaskedTextBox.Size = New-Object System.Drawing.Size(150,20)
+$form_PWD.Add_Shown({$MaskedTextBox.Select()})
+$form_PWD.Controls.Add($MaskedTextBox)
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = "100,120"
+$OKButton.Size = '100,30'
+$OKButton.Text = "Ok"
+$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form_PWD.AcceptButton = $OKButton
+$form_PWD.Controls.Add($OKButton)
 
+$result = $form_PWD.ShowDialog()
+
+[reflection.assembly]::LoadWithPartialName("System.DirectoryServices.AccountManagement") > $null
 $principalContext = [System.DirectoryServices.AccountManagement.PrincipalContext]::new([System.DirectoryServices.AccountManagement.ContextType]'Machine',$env:COMPUTERNAME)
+
 $principalContext.ValidateCredentials($textBox.Text,$MaskedTextBox.Text)
 
 Pause
