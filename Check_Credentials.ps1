36d9560bd5f69bbe577b9e53d6ab3d5244e49398
Dario CORRADA <dario.corrada@agmsolutions.net>
2021-01-09 18:00:17 +0100

    Check_Credentials: checkbox for domain account

--- Check_Credentials.ps1
+++ Check_Credentials.ps1
@@ -1,4 +1,4 @@
-# this is a test script to check account credentials
+# this is a script to check account credentials
 
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
@@ -6,10 +6,10 @@ Add-Type -AssemblyName PresentationFramework
 $workdir = Get-Location
 Import-Module -Name "$workdir\Modules\Forms.psm1"
 
-# NOTE: username needs to be typed as "[domain\]username"
+# get credentials
 $form_PWD = New-Object System.Windows.Forms.Form
 $form_PWD.Text = "LOGIN"
-$form_PWD.Size = "400,250"
+$form_PWD.Size = "400,270"
 $form_PWD.StartPosition = 'CenterScreen'
 $form_PWD.Topmost = $true
 $label = New-Object System.Windows.Forms.Label
@@ -38,21 +38,37 @@ $MaskedTextBox.Location = New-Object System.Drawing.Point(130,80)
 $MaskedTextBox.Size = New-Object System.Drawing.Size(150,20)
 $form_PWD.Add_Shown({$MaskedTextBox.Select()})
 $form_PWD.Controls.Add($MaskedTextBox)
+$CheckBox = New-Object System.Windows.Forms.CheckBox
+$CheckBox.Location = New-Object System.Drawing.Point(20,120)
+$CheckBox.Size = New-Object System.Drawing.Size(200,20)
+$CheckBox.Checked = $false
+$CheckBox.Text = "Domain Account"
+$form_PWD.Add_Shown({$CheckBox.Select()})
+$form_PWD.Controls.Add($CheckBox)
 $OKButton = New-Object System.Windows.Forms.Button
-$OKButton.Location = "100,120"
+$OKButton.Location = "100,160"
 $OKButton.Size = '100,30'
 $OKButton.Text = "Ok"
 $OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $form_PWD.AcceptButton = $OKButton
 $form_PWD.Controls.Add($OKButton)
-
 $result = $form_PWD.ShowDialog()
+
+# get domain name
+$thiscomputer = Get-WmiObject -Class Win32_ComputerSystem
+
+if ($CheckBox.Checked) {
+    $usr = $thiscomputer.Domain + '\' + $textBox.Text
+} else {
+    $usr = $textBox.Text
+}
+$pwd = $MaskedTextBox.Text
 
 [reflection.assembly]::LoadWithPartialName("System.DirectoryServices.AccountManagement") > $null
 $principalContext = [System.DirectoryServices.AccountManagement.PrincipalContext]::new([System.DirectoryServices.AccountManagement.ContextType]'Machine',$env:COMPUTERNAME)
 
 
-if ($principalContext.ValidateCredentials($textBox.Text,$MaskedTextBox.Text)) {
+if ($principalContext.ValidateCredentials($usr,$pwd)) {
     Write-Host -ForegroundColor Green "ACCESS GRANTED!"
 } else {
     Write-Host -ForegroundColor Red "ACCESS DENIED!"
