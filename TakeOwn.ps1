a2a58210517fa4abdba962a3cfe335435b18ddf2
Dario CORRADA <dario.corrada@agmsolutions.net>
2021-05-20 15:24:11 +0200

    TakeOwn: initial release

---
+++ TakeOwn.ps1
@@ -0,0 +1,44 @@
+<#
+Name......: TakeOwn.ps1
+Version...: 21.05.1
+Author....: Dario CORRADA
+
+This script takes ownership of a specific path, recursively
+#>
+
+# elevated script execution with admin privileges
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+if ($testadmin -eq $false) {
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    exit $LASTEXITCODE
+}
+
+# header 
+$ErrorActionPreference= 'SilentlyContinue'
+Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy Bypass -Force
+Write-Host "ExecutionPolicy Bypass" -fore Green
+$ErrorActionPreference= 'Inquire'
+$WarningPreference = 'SilentlyContinue'
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+Add-Type -AssemblyName PresentationFramework
+
+# get target path
+[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+$foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+$foldername.rootfolder = "MyComputer"
+$foldername.ShowDialog()
+$target = $foldername.SelectedPath
+
+$username = $env:USERNAME
+
+# change ownership
+$opts = ("/f", $target, "/r")
+takeown @opts
+
+# grant privileges
+$opts = ($target, "/grant", "$username\:f", "/c", "/t")
+icacls @opts
+
+Pause
