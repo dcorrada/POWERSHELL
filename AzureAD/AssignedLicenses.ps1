f86d89c5c2902bee418f73a6a47e17c81b538743
Dario CORRADA <dario.corrada@agmsolutions.net>
2021-08-03 15:54:43 +0200

    AssignedLicenses: first release

--- AzureAD/AssignedLicenses.ps1
+++ AzureAD/AssignedLicenses.ps1
@@ -35,76 +35,48 @@ $ErrorActionPreference= 'Inquire'
 # connect to Tenant
 Connect-MsolService
 
+# get all accounts available 
+#Get-MsolAccountSku
+
 # retrieve all users that are licensed
 $Users = Get-MsolUser -All | Where-Object { $_.isLicensed -eq "TRUE" } | Sort-Object DisplayName
-
-Get-MsolAccountSku
-
-
-
-
-
-
-
-Connect-AzureAD
 
 # initialize dataframe for collecting data
 $local_array = @()
 
-# retrieve data from AzureAD
-$userlist = Get-AzureADUser -All $true
-$tot = $userlist.Count
+$tot = $Users.Count
 $usrcount = 0
-$ErrorActionPreference= 'SilentlyContinue'
-foreach ($user in $userlist) {
+foreach ($User in $Users) {
     $usrcount ++
     Clear-Host
     Write-Host "Processing $usrcount users out of $tot..."
 
-    # user data
-    $fullname = $user.DisplayName
-    $user.Mail -match "(.+)@agmsolutions\.net$" > $null
-    $username = $matches[1]
+    $username = $User.UserPrincipalName
+    $fullname = $User.DisplayName
 
-	
-
-
-
-    Get-AzureAdUser | ForEach { $licensed=$True ; For ($i=0; $i -le ($_.AssignedLicenses | Measure).Count ; $i++) { If( [string]::IsNullOrEmpty(  $_.AssignedLicenses[$i].SkuId ) -ne $True) { $licensed=$true } } ; If( $licensed -eq $true) { Write-Host $_.UserPrincipalName} }
-
-
-
-      
-            # initialize record for collecting data
-            $local_hash = [ordered]@{ 
-                Fullname = $fullname;
-                Username = $username;
+    $licenses = (Get-MsolUser -UserPrincipalName $username).Licenses.AccountSku | Sort-Object SkuPartNumber
+    if ($licenses.Count -ge 1) { # at least one license
+        $licenselist = $licenses[0].SkuPartNumber
+        if ($licenses.Count -gt 1) {
+            for ($i = 1; $i -lt $licenses.Count; $i++) {
+                $licenselist = $licenselist + ':' + $licenses[$i].SkuPartNumber
             }
+        }
 
-            # update dataframe
-            $local_array += $local_hash
+        # initialize record for collecting data
+        $local_hash = [ordered]@{ 
+            Fullname = $fullname;
+            Username = $username;
+            Licenses = $licenselist
+        }
 
-
-
-            # da valutare
-            Import-Module MSOnline
-            Connect-MsolService
-            Get-MsolUser -All | where {$_.isLicensed -eq $true}
-            Get-MsolAccountSku # fornisce gli account disponibili
-
-            # v. https://www.thelazyadministrator.com/2018/03/19/get-friendly-license-name-for-all-users-in-office-365-using-powershell/
-
-
-
-
+        # update dataframe
+        $local_array += $local_hash
+    }
 }
-$ErrorActionPreference= 'Inquire'
-
-# disconnect from AzureAD
-Disconnect-AzureAD
 
 # output dataframe to a CSV file
-$outfile = "C:\Users\$env:USERNAME\Desktop\AzureAD.csv"
+$outfile = "C:\Users\$env:USERNAME\Desktop\Licenses.csv"
 Write-Host -NoNewline "Writing to $outfile... "
 
 $header = @($local_array[0].Keys)
