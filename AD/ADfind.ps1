93592157e942fe4c32aaca77197aa7f35713df2b
Dario CORRADA <dario.corrada@agmsolutions.net>
2022-06-09 11:01:32 +0200

    Updates

---
+++ AD/ADfind.ps1
@@ -0,0 +1,35 @@
+﻿<#
+Name......: ADfind.ps1
+Version...: 22.06.1
+Author....: Dario CORRADA
+
+Loooking for a PC at Active Directory
+#>
+
+# header 
+$ErrorActionPreference= 'SilentlyContinue'
+Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy Bypass -Force
+Write-Host "ExecutionPolicy Bypass" -fore Green
+$ErrorActionPreference= 'Inquire'
+$WarningPreference = 'SilentlyContinue'
+
+# import Active Directory module
+$ErrorActionPreference= 'Stop'
+try {
+    Import-Module ActiveDirectory
+} catch {
+    Add-WindowsCapability -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0 -Online
+    Import-Module ActiveDirectory
+}
+$ErrorActionPreference= 'Inquire'
+
+$computer_name = Read-Host "Hostname"
+
+$ErrorActionPreference= 'Stop'
+try {
+    Get-ADComputer -Identity $computer_name -Properties *
+} catch {
+    Write-Host -ForegroundColor Red "[$computer_name] not found"
+}
+$ErrorActionPreference= 'Inquire'
+Pause
--- AD/README.txt
+++ AD/README.txt
@@ -1,4 +1,16 @@
 These scripts require Powershell ActiveDirectory module
 
+Add the following chunck to your codes:
+
+# import Active Directory module
+$ErrorActionPreference= 'Stop'
+try {
+    Import-Module ActiveDirectory
+} catch {
+    Add-WindowsCapability -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0 -Online
+    Import-Module ActiveDirectory
+}
+$ErrorActionPreference= 'Inquire'
+
 If not already installed, download RSAT package (Remote Server Administration Tools)
 available on https://www.microsoft.com/en-us/download/details.aspx?id=45520
