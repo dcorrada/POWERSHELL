7181f5ff9702c5bb2c93edb433095e2aecfc308d
Dario CORRADA <dario.corrada@gmail.com>
2023-01-03 17:50:16 +0100

    Forms.psm1: updates

--- Modules/Forms.psm1
+++ Modules/Forms.psm1
@@ -1,4 +1,4 @@
-﻿# function to create dialogs windows
+﻿# functions to create dialogs
 
 function FormBase {
     param ($w, $h, $text)
@@ -13,27 +13,12 @@ function FormBase {
 }
 Export-ModuleMember -Function FormBase
 
-function RadioButton {
-    param ($form, $checked, $enabled = $true, $x, $y, $text)
-
-    $obj = New-Object System.Windows.Forms.RadioButton
-    $obj.Location = "$x,$y"
-    $obj.Size = '300,30'
-    $obj.Checked = $checked
-    $obj.Enabled = $enabled
-    $obj.Text = $text
-    $form.Controls.Add($obj)
-
-    return $obj
-}
-Export-ModuleMember -Function RadioButton
-
 function CheckBox {
-    param ($form, $checked, $enabled = $true, $x, $y, $text)
+    param ($form, $checked, $enabled = $true, $x, $y, $w = 200, $h = 30, $text)
 
     $obj = New-Object System.Windows.Forms.CheckBox
     $obj.Location = "$x,$y"
-    $obj.Size = '350,30'
+    $obj.Size = "$w,$h"
     $obj.Checked = $checked
     $obj.Enabled = $enabled
     $obj.Text = $text
@@ -43,48 +28,8 @@ function CheckBox {
 }
 Export-ModuleMember -Function CheckBox
 
-function OKButton {
-    param ($form, $x, $y, $text)
-
-    $obj = New-Object System.Windows.Forms.Button
-    $obj.Location = "$x,$y"
-    $obj.Size = '100,30'
-    $obj.Text = $text
-    $obj.DialogResult = [System.Windows.Forms.DialogResult]::OK
-    $form.AcceptButton = $obj
-    $form.Controls.Add($obj)
-}
-Export-ModuleMember -Function OKButton
-
-
-function TxtBox {
-    param ($form, $x, $y, $w, $h, $text)
-
-    $obj = New-Object System.Windows.Forms.TextBox
-    $obj.Location = New-Object System.Drawing.Point($x,$y)
-    $obj.Size = New-Object System.Drawing.Size($w,$h)
-    $obj.Text = $text
-    $form.Controls.Add($obj)
-
-    return $obj
-}
-Export-ModuleMember -Function TxtBox
-
-function Label {
-    param ($form, $x, $y, $w, $h, $text)
-
-    $obj = New-Object System.Windows.Forms.Label
-    $obj.Location = New-Object System.Drawing.Point($x,$y)
-    $obj.Size = New-Object System.Drawing.Size($w,$h)
-    $obj.Text = $text
-    $form.Controls.Add($obj)
-
-    return $obj
-}
-Export-ModuleMember -Function Label
-
 function DropDown {
-    param ($form, $x, $y, $w, $h, [String[]]$opts)
+    param ($form, $x, $y, $w = 200, $h = 30, [String[]]$opts)
 
     $obj = new-object System.Windows.Forms.ComboBox
     $obj.Location = New-Object System.Drawing.Point($x,$y)
@@ -99,31 +44,62 @@ function DropDown {
 }
 Export-ModuleMember -Function DropDown
 
-function ProgressBar {
-    $form_bar = New-Object System.Windows.Forms.Form
-    $form_bar.Text = "PROGRESS"
-    $form_bar.Size = New-Object System.Drawing.Size(600,200)
-    $form_bar.StartPosition = 'CenterScreen'
-    $form_bar.Topmost = $true
-    $form_bar.MinimizeBox = $false
-    $form_bar.MaximizeBox = $false
-    $form_bar.FormBorderStyle = 'FixedSingle'
-    $font = New-Object System.Drawing.Font("Arial", 12)
-    $form_bar.Font = $font
-    $label = New-Object System.Windows.Forms.Label
-    $label.Location = New-Object System.Drawing.Point(20,20)
-    $label.Size = New-Object System.Drawing.Size(550,30)
-    $form_bar.Controls.Add($label)
-    $bar = New-Object System.Windows.Forms.ProgressBar
-    $bar.Style="Continuous"
-    $bar.Location = New-Object System.Drawing.Point(20,70)
-    $bar.Maximum = 101
-    $bar.Size = New-Object System.Drawing.Size(550,30)
-    $form_bar.Controls.Add($bar)
-    $form_bar.Show() | out-null
-    return @($form_bar, $bar, $label)
+function Label {
+    param ($form, $x, $y, $w = 200, $h = 30, $text)
+
+    $obj = New-Object System.Windows.Forms.Label
+    $obj.Location = "$x,$y"
+    $obj.Size = "$w,$h"
+    $obj.Text = $text
+    $form.Controls.Add($obj)
+
+    return $obj
 }
-Export-ModuleMember -Function ProgressBar
+Export-ModuleMember -Function Label
+
+function RadioButton {
+    param ($form, $checked, $enabled = $true, $x, $y, $w = 200, $h = 30, $text)
+
+    $obj = New-Object System.Windows.Forms.RadioButton
+    $obj.Location = "$x,$y"
+    $obj.Size = "$w,$h"
+    $obj.Checked = $checked
+    $obj.Enabled = $enabled
+    $obj.Text = $text
+    $form.Controls.Add($obj)
+
+    return $obj
+}
+Export-ModuleMember -Function RadioButton
+
+function TxtBox {
+    param ($form, $x, $y, $w = 200, $h = 30, $text, $masked = $false)
+
+    $obj = New-Object System.Windows.Forms.TextBox
+    $obj.Location = "$x,$y"
+    $obj.Size = "$w,$h"
+    $obj.Text = $text
+    if ($masked) {
+        $obj.PasswordChar = '*'
+    }
+    $form.Controls.Add($obj)
+
+    return $obj
+}
+Export-ModuleMember -Function TxtBox
+
+function OKButton {
+    param ($form, $x, $y, $w = 100, $h = 30, $text)
+
+    $obj = New-Object System.Windows.Forms.Button
+    $obj.Location = "$x,$y"
+    $obj.Size = "$w,$h"
+    $obj.Text = $text
+    $obj.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.AcceptButton = $obj
+    $form.Controls.Add($obj)
+}
+Export-ModuleMember -Function OKButton
 
 function LoginWindow {
     $form_PWD = New-Object System.Windows.Forms.Form
@@ -177,3 +153,30 @@ function LoginWindow {
     return $login
 }
 Export-ModuleMember -Function LoginWindow
+
+function ProgressBar {
+    $form_bar = New-Object System.Windows.Forms.Form
+    $form_bar.Text = "PROGRESS"
+    $form_bar.Size = New-Object System.Drawing.Size(600,200)
+    $form_bar.StartPosition = 'CenterScreen'
+    $form_bar.Topmost = $true
+    $form_bar.MinimizeBox = $false
+    $form_bar.MaximizeBox = $false
+    $form_bar.FormBorderStyle = 'FixedSingle'
+    $font = New-Object System.Drawing.Font("Arial", 12)
+    $form_bar.Font = $font
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Point(20,20)
+    $label.Size = New-Object System.Drawing.Size(550,30)
+    $form_bar.Controls.Add($label)
+    $bar = New-Object System.Windows.Forms.ProgressBar
+    $bar.Style="Continuous"
+    $bar.Location = New-Object System.Drawing.Point(20,70)
+    $bar.Maximum = 101
+    $bar.Size = New-Object System.Drawing.Size(550,30)
+    $form_bar.Controls.Add($bar)
+    $form_bar.Show() | out-null
+    return @($form_bar, $bar, $label)
+}
+Export-ModuleMember -Function ProgressBar
+
