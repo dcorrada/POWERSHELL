794477230a2b5e6d922c0482e749071db8087293
Dario CORRADA <dario.corrada@gmail.com>
2024-09-24 14:30:12 +0200

    Init_PC.ps1: notes

--- Init_PC.ps1
+++ Init_PC.ps1
@@ -124,6 +124,17 @@ foreach ($item in ($swlist.Keys | Sort-Object)) {
             } else {
                 Write-Host -ForegroundColor Red " FAILED"
                 [System.Windows.MessageBox]::Show("Something has gone wrong, check the file `n[$winget_stdout_file]",'OOOPS!','Ok','Error') | Out-Null
+                 <#
+                    On older PC error 3010 can occur during installation: in most cases Acrobat 
+                    seems to correctly run without any expected crash or further fails.
+
+                    In the worst scenario try the following steps and see if that works for you:
+                      * Run the Acrobat cleaner tool 
+                        https://labs.adobe.com/downloads/acrobatcleaner.html
+                      * Reboot the computer
+                      * Reinstall the application using the link 
+                        https://helpx.adobe.com/in/download-install/kb/acrobat-downloads.html
+                #>
             }
         } elseif ($item -eq 'Chrome') {
             <# 
