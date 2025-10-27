4b87180db2fcd050eba2bfc77f77d2c50907874c
Dario CORRADA <dario.corrada@gmail.com>
2025-10-27 17:41:00 +0100

    Robocopycat.ps1: initializing trees

--- Robocopycat/Robocopycat.ps1
+++ Robocopycat/Robocopycat.ps1
@@ -63,6 +63,7 @@ $ErrorActionPreference= 'Inquire'
 <# *******************************************************************************
                                     INIT
 ******************************************************************************* #>
+Write-Host -ForegroundColor Cyan -NoNewline "Looking for source tree and replicate onto destination"
 
 # paths
 [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") > $null
@@ -71,19 +72,55 @@ $foldername.RootFolder = "MyComputer"
 $foldername.ShowNewFolderButton = $false
 $foldername.Description = "SOURCE FOLDER"
 $foldername.ShowDialog() > $null
-$SOURCEpath = $foldername.SelectedPath
+$SOURCEpath = $foldername.SelectedPath -replace '\\', '/'
 [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") > $null
 $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
 $foldername.RootFolder = "MyComputer"
 $foldername.ShowNewFolderButton = $false
 $foldername.Description = "DESTINATION FOLDER"
 $foldername.ShowDialog() > $null
-$DESTpath = $foldername.SelectedPath
+$DESTpath = $foldername.SelectedPath -replace '\\', '/'
 
+# RETRIEVING SOURCE TREE
+# the number of subfolders found will define the amount of independent robocopy
+# jobs that will be parallelized
+$sourceTree = @{}
+# the deepSeek value specify how much deeply looking for subfolders recursively
+$deepSeek = 10
+$ErrorActionPreference= 'SilentlyContinue'
+$sourceTree[$SOURCEpath] = 0
+for ($i = 1; $i -lt $deepSeek; $i++) {
+    foreach ($parent in $sourceTree.Keys) {
+        if ($sourceTree[$parent] -eq ($i - 1)) {
+            foreach ($child in Get-ChildItem -Path $parent) {
+                if ($child.Mode -eq 'd-----') { # new subfolder
+                    $astring = $parent + '/' + $child.Name
+                    $sourceTree[$astring] = $i
+                    Write-Host -NoNewline '.'
+                }
+            }
+        }
+    }
+}
+$ErrorActionPreference= 'Inquire'
 
+# GENERATING DESTINATION TREE
+$destTree = @{}
+foreach ($item in $sourceTree.Keys) {
+    $adest = $item -replace "$SOURCEpath", "$DESTpath"
+    $destTree[$adest] = $sourceTree[$item]
+    Write-Host -NoNewline '.'
+}
 
-# fare dei test di questo comando su un albero poco ramificato contenente file e sottocartelle
-Get-ChildItem -Path $SOURCEpath -Recurse
+for ($i = 1; $i -lt $deepSeek; $i++) {
+    foreach ($item in $destTree.Keys) {
+        if ($destTree[$item] -eq $i) {
+            New-Item -Path $item -ItemType Directory | Out-Null
+            Write-Host -NoNewline '.'
+        }
+    }
+}
+Write-Host -ForegroundColor Green " Done`n"
 
 
 <#
@@ -91,30 +128,9 @@ Get-ChildItem -Path $SOURCEpath -Recurse
 +++  NOTE PER ME  +++
 +++++++++++++++++++++
 
-* Funzione ricorsiva per creare un hash contenente tutte le cartelle e le sottocartelle da copiare
-
 * Gestire un file exclude list per i path da escludere, verificare se in robocopy esiste una opzione exclude
 
 * Rimuovere, dalla stringa del job di robocopy, l'opzione di girare ricorsivamente nelle sottocartelle 
+
+* Togliere da GitHub il source tree di testing [unstable]/Robocopycat/RoboCopyCat_TEST
 #>
-
-
-
-
-
-
-
-
-
-<#
-Tutto cio' a valle di questo commeto e' da considerarsi temporaneo. 
-Una volta ultimato per la release iniziale di testing cancellare le righe da 
-qui in giu' e spsotare lo script sul [unstable]/Locale
-#>
-
-
-Pause
-Clear-Host
-Write-Host -ForegroundColor Cyan  -NoNewline  "[$workdir] "
-Write-Host -ForegroundColor Yellow "HelloWorld!"
-Exit 
