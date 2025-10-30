efea3e2589c3750a3a014d136d030cac15615ed2
Dario CORRADA <dario.corrada@gmail.com>
2025-10-30 16:19:15 +0100

    AGMskyline: new SQL query

---
+++ miscellaneous/AGMskyline/TrendMicro_zombie.sql
@@ -0,0 +1,13 @@
+/*
+Questa query incrocia gli asset estratti da SnipeIT con gli hostname indicati 
+su TrendMicro allo scopo di individuare se ci fosse qualche liceenza da 
+dismettere (ad es. se quell'hostname punti ad un asset non piu' assegnato).
+*/
+pager less -SFX;
+
+USE AGMskyline;
+
+SELECT TrendMicroparsed.HOSTNAME, EstrazioneAsset.STATUS
+FROM TrendMicroparsed
+LEFT JOIN EstrazioneAsset 
+ON TrendMicroparsed.HOSTNAME = EstrazioneAsset.HOSTNAME
