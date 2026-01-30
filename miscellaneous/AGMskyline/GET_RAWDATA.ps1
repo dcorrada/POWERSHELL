079ba3d22c28842aeeba390aa42b6677d0f0ee6f
Dario CORRADA <dario.corrada@gmail.com>
2026-01-30 14:14:09 +0100

    AGMskyline - new table ConfigManager

--- miscellaneous/AGMskyline/GET_RAWDATA.ps1
+++ miscellaneous/AGMskyline/GET_RAWDATA.ps1
@@ -38,7 +38,7 @@ Import-Module -Name "$workdir\Modules\Forms.psm1"
 
 # tables dialog
 $swlist = @()
-$form_panel = FormBase -w 350 -h 580 -text "TABLES"
+$form_panel = FormBase -w 350 -h 610 -text "TABLES"
 $swlist += CheckBox -form $form_panel -checked $true -x 20 -y 20 -text "ADcomputers"
 $swlist += CheckBox -form $form_panel -checked $true -x 20 -y 50 -text "ADusers"
 $swlist += CheckBox -form $form_panel -checked $true -x 20 -y 80 -text "AzureDevices" # migrare codice su Graph
@@ -47,14 +47,15 @@ $swlist += CheckBox -form $form_panel -checked $true -x 20 -y 140 -text "DLmembe
 $swlist += CheckBox -form $form_panel -checked $false -enabled $false -x 20 -y 170 -text "DymoLabel" # tabella redatta manualmente
 $swlist += CheckBox -form $form_panel -checked $true -x 20 -y 200 -text "EstrazioneAsset"
 $swlist += CheckBox -form $form_panel -checked $true -x 20 -y 230 -text "EstrazioneUtenti"
-$swlist += CheckBox -form $form_panel -checked $true -x 20 -y 260 -text "o365licenses" # migrare codice su Graph
-$swlist += CheckBox -form $form_panel -checked $false -enabled $false -x 20 -y 290 -text "o365licenses_nologin"
-$swlist += CheckBox -form $form_panel -checked $true -x 20 -y 320 -text "PwdExpire"
-$swlist += CheckBox -form $form_panel -checked $true -x 20 -y 350 -text "SchedeAssunzione" 
-$swlist += CheckBox -form $form_panel -checked $false -enabled $false -x 20 -y 380 -text "ThirdPartiesLicenses" # tabella redatta manualmente
-$swlist += CheckBox -form $form_panel -checked $true -x 20 -y 410 -text "TrendMicroparsed"
-$swlist += CheckBox -form $form_panel -checked $false -enabled $false -x 20 -y 440 -text "Xrefs" # tabelle create in fase di update
-OKButton -form $form_panel -x 100 -y 490 -text "Ok"  | Out-Null
+$swlist += CheckBox -form $form_panel -checked $true -x 20 -y 260 -text "ConfigManager"
+$swlist += CheckBox -form $form_panel -checked $true -x 20 -y 290 -text "o365licenses" # migrare codice su Graph
+$swlist += CheckBox -form $form_panel -checked $false -enabled $false -x 20 -y 320 -text "o365licenses_nologin"
+$swlist += CheckBox -form $form_panel -checked $true -x 20 -y 350 -text "PwdExpire"
+$swlist += CheckBox -form $form_panel -checked $true -x 20 -y 380 -text "SchedeAssunzione" 
+$swlist += CheckBox -form $form_panel -checked $false -enabled $false -x 20 -y 410 -text "ThirdPartiesLicenses" # tabella redatta manualmente
+$swlist += CheckBox -form $form_panel -checked $true -x 20 -y 440 -text "TrendMicroparsed"
+$swlist += CheckBox -form $form_panel -checked $false -enabled $false -x 20 -y 470 -text "Xrefs" # tabelle create in fase di update
+OKButton -form $form_panel -x 100 -y 520 -text "Ok"  | Out-Null
 $form_panel.ShowDialog() | Out-Null
 
 foreach ($item in $swlist) {
